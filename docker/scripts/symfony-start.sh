2718413c175b359b0e8e0c87dd1259b51cb9978d
Hernán Bozán <hernan.bozan@gmail.com>
2024-10-16 21:13:14 -0400

    Improvements for Symfony install

--- docker/scripts/symfony-start.sh
+++ docker/scripts/symfony-start.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 set -e
 
-echo "Versión solicitada: ${SYMFONY_VERSION:-última versión estable}"
+echo "Using Symfony Version: ${SYMFONY_VERSION}"
+echo "Using PHP Version: ${PHP_VERSION}"
 
 # Checking if Symfony's project exist
 if [ -f "composer.json" ]; then
   echo "There is already a Symfony project. Skip installation."
 else
-#  echo "Symfony no está instalado. Creando proyecto Symfony..."
-#  echo "Versión solicitada: ${SYMFONY_VERSION:-última versión estable}"
 
-  # Crear proyecto Symfony temporalmente en 'tmp'
-  composer create-project "symfony/skeleton" tmp --prefer-dist --no-progress --no-interaction --no-install || {
+  # Create temporary in tmp file
+  composer create-project "symfony/skeleton ${SYMFONY_VERSION}" tmp --prefer-dist --no-progress --no-interaction --no-install || {
     echo "Error: Download of Symfony Skeleton failed."
     exit 1
   }
@@ -21,6 +20,10 @@ else
   cp -Rp . ..
   cd ..
   rm -rf tmp/
+
+  #Add extras to composer.json
+  composer require "php:>=${PHP_VERSION}"
+	composer config --json extra.symfony.docker 'true'
 
   echo "Symfony's skeleton correctly installed."
 fi
